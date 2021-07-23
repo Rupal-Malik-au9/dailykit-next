eeb75892f0529d08010cd86b395d8c57ceb5f5cc
rupal <rupalmalik3@gmail.com>
2021-07-23 16:13:09 +0530

    fix: routing to card-details page

--- components/Layout_2/index.jsx
+++ components/Layout_2/index.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { useRouter } from "next/router";
 import { useAuth } from "../../store/auth";
-import Header from "../sections/Header";
+import Navbar from "../Navbar";
 function Redirect({ to }) {
   const router = useRouter();
   React.useEffect(() => {
@@ -11,12 +11,10 @@ function Redirect({ to }) {
 }
 export const Layout_2 = ({ children }) => {
   const { authenticated } = useAuth();
-
   if (!authenticated) <Redirect to="/onboard/login" />;
-
   return (
     <>
-      <Header />
+      <Navbar />
       <main>{children}</main>
     </>
   );
--- components/PriceHeading.js
+++ components/PriceHeading.js
@@ -3,12 +3,12 @@ import React, { PureComponent } from "react";
 export default class PriceHeading extends PureComponent {
   render() {
     return (
-      <div class="container">
-        <div class="row justify-content-center">
-          <div class="col-md-10 col-xs-12 text-center">
-            <div class="sec-heading">
-              <h2 class="sec-title-lg">{this.props.heading}</h2>
-              <p class="sec-text-pad lead">
+      <div className="container">
+        <div className="row justify-content-center">
+          <div className="col-md-10 col-xs-12 text-center">
+            <div className="sec-heading">
+              <h2 className="sec-title-lg">{this.props.heading}</h2>
+              <p className="sec-text-pad lead">
                 <b>
                   The most powerful Restaurant Automation Software on the
                   planet. If you just want as many sales avenues as possible or
